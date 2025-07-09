20336dafc4127c201edde521acb2d2c78e45a623
Reeman Abd Alqader <eng.reeman.m.a@gmail.com>
2025-07-09 18:34:52 +0300

    Create  Mahmoud and Ehab and the even-odd game.cpp

---
+++ Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,19 @@
+https://codeforces.com/problemset/problem/959/A//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include<set>
+#include <map>
+#define ll long long 
+using namespace std;
+int main  (){
+int n ;
+cin >> n ;
+if (n %2==0)
+cout <<"Mahmoud"<<endl ;
+else 
+cout << "Ehab"<<endl ;
+
+return 0;
+} 
