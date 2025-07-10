5d04b552de543bb42d34512dd7d7a51b6f7cc26b
Reeman Abd Alqader <eng.reeman.m.a@gmail.com>
2025-07-10 19:02:05 +0300

    Create Ichihime and Triangle.cpp

---
+++ Ichihime and Triangle.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/problemset/problem/1337/A 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include<set>
+#include <map>
+#define ll long long 
+using namespace std;
+int main  (){
+  ll t ;
+  cin >> t;
+  while (t--){
+    ll a,b,c,d ;
+    cin >>a>>b>>c>>d ;
+    cout <<b <<" "<< c<<" "<<c<<endl ;
+  }
+
+return 0;
+} 
