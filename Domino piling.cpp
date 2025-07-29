e8de7dea9abac784a1e3a9f9896b77052739da02
Reeman Abd Alqader <eng.reeman.m.a@gmail.com>
2025-07-29 10:08:47 +0300

    Create Domino piling.cpp

---
+++ Domino piling.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/problemset/problem/50/A
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include<set>
+#include <map>
+#define ll long long 
+using namespace std;
+int main  () {
+int n ,m ;
+cin >> n>>m  ;
+int g =n*m ;
+cout << g/2 << endl  ;
+ 
+return 0;
+}
