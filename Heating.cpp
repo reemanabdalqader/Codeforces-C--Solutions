a38728d3437ef6b1288cc64dc3d7fc8e6f1e75f0
Reeman Abd Alqader <eng.reeman.m.a@gmail.com>
2025-10-04 17:38:10 +0300

    Create Heating.cpp

---
+++ Heating.cpp
@@ -0,0 +1,37 @@
+//https://codeforces.com/problemset/problem/1260/A
+
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#define ll long long
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(nullptr); 
+int t ;
+cin >> t ;
+while (t--){
+    ll c , sum ;
+    cin >> c >> sum ;
+    vector<ll > vec (c,0);
+    ll num =0 ;
+    while (sum--){
+        vec[num]++;
+        num ++;
+        if (num==c )num=0 ;
+        
+    }
+    ll ans =0;
+    for (int i=0;i<c ;i++){
+ans +=pow(vec [i],2);
+    }
+cout << ans << endl ;
+}
+return 0;
+
+}
