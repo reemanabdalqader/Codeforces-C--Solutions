9873002e4444f9bc6141c3a4053b827077765411
Reeman Abd Alqader <eng.reeman.m.a@gmail.com>
2026-01-31 21:16:14 +0200

    Update 1740A Factorise N+M.cpp

--- 1740A Factorise N+M.cpp
+++ 1740A Factorise N+M.cpp
@@ -43,3 +43,17 @@ count ++ ;
   }}
  return 0;
   }
+//----------------------------------------------------------------------------
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << n << "\n";
+    }
+    return 0;
+}
